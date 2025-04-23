fb067cddcff72733c19c46e9eca6f27720425cb3
CarlHuang <104048874+CarlHuangNuc@users.noreply.github.com>
2025-04-24 07:54:22 +0800

    Update __init__.py

--- distrl/environment/android/__init__.py
+++ distrl/environment/android/__init__.py
@@ -1,3 +1,3 @@
 from .env import BatchedAndroidEnv
 from .evaluate import EndResultEvaluator
-from .autoui_utils import cogagent_translate_action, autoui_translate_action, autoui_prepare_prompt
+from .autoui_utils import autoui_translate_action, autoui_prepare_prompt
